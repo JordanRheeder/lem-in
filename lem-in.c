20a6afd61a82475171f1173d787d334c53983e1f
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-05-02 11:30:46 +0200

    dunno

--- lem-in.c
+++ lem-in.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 11:28:51 by jrheeder          #+#    #+#             */
-/*   Updated: 2019/09/22 18:06:13 by rengelbr         ###   ########.fr       */
+/*   Updated: 2020/05/02 11:29:29 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,17 @@
 int main()
 {
 	char *line;
-	t_log *ret;
-	t_room *maze;
-
-	// int i = 0;
-//	 int j = 0;
-//	int valid;
+	t_str *raw_data;
+	// t_log *ret;
+	// t_room **maze;
 
 	line = NULL;
+	// maze = (t_room **)malloc(sizeof(t_room *));
 printf("reading input\n");
-	ret = read_input(line);				//*****DOES NOT LEAK****
+	raw_data = read_input(line);				//*****DOES NOT LEAK****
+
 printf("input read\n");
-printf("validating input\n");
-	validate(ret);						//******LEAKS A F##K TON****
-printf("input validated\n");
 printf("mapping input\n");
-	maze = init_rooms(ret);				//*****SOMEWHAT LEAKY******
-printf("input initialized\n");
-	// TODO: Perform algorithm
-	// TODO: Print everything
+// 	maze = init_rooms(ret);				//*****SOMEWHAT LEAKY******
+// printf("input initialized\n");
 }
