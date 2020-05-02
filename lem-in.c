be90fabb3a18f67ef450b43d2c9438a95a67a62c
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-05-02 11:41:40 +0200

    wtf is going on

--- lem-in.c
+++ lem-in.c
@@ -6,7 +6,7 @@
 /*   By: rengelbr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 11:28:51 by jrheeder          #+#    #+#             */
-/*   Updated: 2020/05/02 11:29:29 by rengelbr         ###   ########.fr       */
+/*   Updated: 2020/05/02 11:39:36 by rengelbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,11 @@ int main()
 	// maze = (t_room **)malloc(sizeof(t_room *));
 printf("reading input\n");
 	raw_data = read_input(line);				//*****DOES NOT LEAK****
-
+	int i = 0;
+	while (raw_data[i]) {
+		printf("%s\n", raw_data[i]);
+		i++;
+	}
 printf("input read\n");
 printf("mapping input\n");
 // 	maze = init_rooms(ret);				//*****SOMEWHAT LEAKY******
